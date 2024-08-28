3d116589fe202974bd91c712a7af50aea360a948
Abhay Jirati <abhi.jrt12@gmail.com>
2024-08-28 20:16:17 +0530

    adding the MainLayout and Header Section

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
